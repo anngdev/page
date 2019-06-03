c6fa072c84ee8cd4b7a55485ea62d16c1a27ee80
An <anngdev@gmail.com>
2019-06-03 22:48:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function a875x() {
-  console.log("load js github");
+  var img = document.createElement('img');
+  img.src = 'https://genknews.genkcdn.vn/2014/0-7d71d.jpg';
+  var center = document.querySelector("#app");
+  center.parentNode.replaceChild(img, center)
 }
