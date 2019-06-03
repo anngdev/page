f03e6e9f235048981d773d8c6a875a918d69cc8a
An <anngdev@gmail.com>
2019-06-03 22:59:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function a875x() {
   var img = document.createElement('img');
+  img.align= "middle"
   img.src = 'https://genknews.genkcdn.vn/2014/0-7d71d.jpg';
-  var center = document.querySelector("#app");
-  center.parentNode.replaceChild(img, center)
+  var center = document.createElement("center");
+  center.appendChild(img)
+  var mainapp = document.querySelector("#app");
+  mainapp.parentNode.replaceChild(center, mainapp)
 }
